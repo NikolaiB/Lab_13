aa68ac4f6eec73da2f997302ba9d1153e073b7f7
NikolaiB <altnikolos95@gmail.com>
2023-02-01 18:38:33 +0300

    Add project files.

---
+++ WebApplication3/Controllers/UserRegisterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class UserRegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RegUser()
+        {
+            return View();
+        }
+        
+
+        public IActionResult Register(string name, int age, string email, string pass, string confPass)
+        {
+            if (pass != confPass)
+            {
+                ViewBag.result = "Error";
+
+                return View();
+            }
+
+            UserCollection model = new UserCollection();
+
+            var user = new UserRegister()
+            {
+                Name = name,
+                Email = email,
+                Age = age,
+                Password = pass,
+                ConfirmPassword = confPass,
+
+            };
+            
+            model.collection.Add(user);
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/UserRegister.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication3.Models
+{
+    public class UserRegister
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class UserCollection
+    {
+        public List<UserRegister> collection { get; set; }
+        public UserCollection()
+        {
+            collection = new List<UserRegister>();
+        }
+    }
+}
